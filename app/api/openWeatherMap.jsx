81ea2d091b3f49307afcbf801341cc84d088ef2a
Artis Coleman <ArtisColemanJr@gmail.com>
2017-04-27 13:11:06 -0400

    Initial working application

---
+++ app/api/openWeatherMap.jsx
@@ -0,0 +1,23 @@
+var axios = require('axios');
+
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=22b2341b045ec0078eb6a66454afbdee&units=imperial';
+
+module.exports = {
+  getTemp: function (location) {
+    var encodedLocation = encodeURIComponent(location);
+    var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
+
+    return axios.get(requestUrl).then(function (res) {
+      debugger;
+      if (res.data.cod && res.data.message) {
+        throw new Error(res.data.message);
+      } else if (res.data.name.toLowerCase() !== location.toLowerCase()) {
+        throw new Error("Invalid Input");
+      } else {
+        return (res.data.main.temp);
+      }
+    }, function (res) {
+      throw new Error(res.data.message);
+    });
+  }
+}
