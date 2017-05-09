0342ff83b3a50de8362496afb717847963c4465b
Artis Coleman <ArtisColemanJr@gmail.com>
2017-05-09 09:50:19 -0400

    Updated the About page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -4,7 +4,7 @@ var About = (props) => {
   return (
     <div>
       <h1 className="text-center page-title">About</h1>
-      <p>This is a weather application build on React. I have built this for The Complete React Web App Developer Course.</p>
+      <p>This is a weather application build using ReactJS and Foundation by Artis Coleman.</p>
       <p>Here are some of the tools I used:</p>
       <ul>
         <li>
